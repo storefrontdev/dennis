39370feabc7ade1461825fbd537a93ecd9b0bd85
Chris Collinsworth <cmcollin41@gmail.com>
2023-02-23 23:56:24 -0700

    dennis

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 
-import  { useCart } from "@/lib/swell/hooks"
-
-
 export default function Home() {
-
-  const { addItem } = useCart()
 
   return (
    <main className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">Dennis Storefront</h1>
     </main>
   )
 }
