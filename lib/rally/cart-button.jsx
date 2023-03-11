2e5864317ec0bbe8152cba6422a2a22e1b11e65b
Chris Collinsworth <cmcollin41@gmail.com>
2023-03-11 15:39:25 -0700

    update

--- lib/rally/cart-button.jsx
+++ lib/rally/cart-button.jsx
@@ -1,14 +1,21 @@
+'use client'
+import { useEffect } from 'react'
+
 import React from 'react'
+import { Rally } from '@/lib/rally/client';
+
+
 
 const CartButton = (props) => {
+
+  useEffect(() => {
+    Rally
+  }, [])
+
   const customClass = props.customClass || "rally-custom-button-class";
- 
+
   return (
-    <rally-checkout-button 
-      suppressHydrationWarning={true} 
-      custom-class={customClass} 
-      custom-text={props.customText} 
-      loader="true">
+    <rally-checkout-button suppressHydrationWarning={true} custom-class={customClass} custom-text={props.customText} loader="true">
     </rally-checkout-button>
   )
 }
